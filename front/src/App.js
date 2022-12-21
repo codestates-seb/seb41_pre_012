1089c4594aabb5b97ca5f0c61343ce674a8acf77
rhino-ty <okko8522@gmail.com>
2022-12-21 10:52:10 +0900

    feat: footer 와이어프레임 짜기

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,15 @@
+import Footer from "./components/Footer";
+import "./App.css";
+import styled from "styled-components";
+
+const AppStyled = styled.div``;
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <AppStyled>
+      <Footer />
+    </AppStyled>
+  );
 }
 
 export default App;
---
+++ front/src/components/Footer.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  width: 100%;
+  height: 320px;
+  background-color: #25262a;
+`;
+
+const Footer = () => {
+  return <StyledFooter></StyledFooter>;
+};
+
+export default Footer;
