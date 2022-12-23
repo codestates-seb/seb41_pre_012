c5651e670af3a793292cd8a2a55e8b4f1b3ffcc3
Jonghui <68373532+hie6953@users.noreply.github.com>
2022-12-23 10:14:56 +0900

    fix: 회원가입 member entity 변수명 변경

--- server/stackoverflow/src/main/java/com/pre012/stackoverflow/member/Member.java
+++ server/stackoverflow/src/main/java/com/pre012/stackoverflow/member/Member.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long uid;
+    private Long mid;
 
     @Column(unique = true, nullable = false)
     private String email;
