5d97c7dd2fab1afa4646e5d0287a6ce6aac791a9
yejin32 <vhdekddl3@naver.com>
2022-12-23 19:14:58 +0900

    style: askquestion 반응형 웹 구현

--- front/src/pages/AskQuestion.js
+++ front/src/pages/AskQuestion.js
@@ -4,8 +4,15 @@ import styled from "styled-components";
 import backimg from "../img/bckImage.svg";
 import { questionCreate } from "../util/questionAPI";
 import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const StyledAskQuestion = styled.div`
+  @media screen and (max-width: 1150px) {
+    img {
+      display: none;
+    }
+  }
+  width: 100%;
   padding: 0 24px 24px 24px;
   background-color: #f8f9f9;
   text-align: left;
@@ -13,12 +20,12 @@ const StyledAskQuestion = styled.div`
   justify-content: center;
 `;
 const QuestionForm = styled.div`
-  width: 1216px;
+  width: 75%;
   height: 1000px;
   margin-bottom: 48px;
 `;
 const QuestionHeader = styled.div`
-  width: 1216px;
+  width: 100%;
   height: 130px;
   display: flex;
   align-items: center;
@@ -26,7 +33,7 @@ const QuestionHeader = styled.div`
 `;
 const QuestionH1 = styled.h1`
   margin: 24px 0px 27px 0;
-  font-size: 27px;
+  font-size: 27px; /* 2vh */
   color: #232629;
   font-weight: 600;
   line-height: 1.3;
@@ -36,13 +43,15 @@ const TitleBox = styled.div`
   height: 160px;
   padding: 24px;
   border: 1px solid #e3e6e8;
+  border-radius: 3px;
   margin-bottom: 20px;
   display: flex;
   flex-direction: column;
+  background-color: #ffffff;
 `;
 
 const Title = styled.div`
-  font-size: 23px;
+  font-size: 22px;
   font-weight: 600;
   padding: 5px 2px;
 `;
@@ -51,6 +60,10 @@ const TitleInput = styled.input`
   padding: 0.6em 0.7em;
   border: 1px solid #babfc4;
   border-radius: 3px;
+  ::placeholder {
+    color: #c9bfc4;
+  }
+
   :focus {
     outline: none !important;
     border-color: #6bbbf7;
@@ -59,15 +72,19 @@ const TitleInput = styled.input`
 `;
 
 const Desc = styled.div`
-  font-size: 15px;
+  font-size: 14px;
   padding: 0 2px;
   margin: 2px 0 8px 0;
 `;
 
 const ProblemBox = styled.div`
-  height: 425px;
+  height: 330px;
   padding: 24px;
   border: 1px solid #e3e6e8;
+  background-color: #ffffff;
+  border-radius: 3px;
+  margin-bottom: 20px;
+
   textarea {
     width: 100%;
     height: 200px;
@@ -75,7 +92,7 @@ const ProblemBox = styled.div`
 `;
 const RegisterBtn = styled.button`
   width: 130px;
-  height: 38px;
+  height: 45px;
   padding: 1px 6px;
   margin: 10px 8px 0 0;
   color: #ffffff;
@@ -90,7 +107,7 @@ const RegisterBtn = styled.button`
 
 const CancleBtn = styled.button`
   width: 97px;
-  height: 38px;
+  height: 45px;
   padding: 1px 6px;
   color: #c22e32;
   border: none;
@@ -143,51 +160,56 @@ const AskQuestion = () => {
   };
 
   return (
-    <StyledAskQuestion>
-      <QuestionForm>
-        <QuestionHeader>
-          <QuestionH1>Ask a public question</QuestionH1>
-          <img src={backimg} alt="backimg"></img>
-        </QuestionHeader>
-        <TitleBox>
-          <Title>Title</Title>
-          <Desc>
-            Be specific and imagine you’re asking a question to another person.
-          </Desc>
-          <TitleInput
-            type="text"
-            value={title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-          ></TitleInput>
-        </TitleBox>
-        <ProblemBox>
-          <Title>What are the details of your problem?</Title>
-          <Desc>
-            Introduce the problem and expand on what you put in the title.
-            Minimum 20 characters.
-          </Desc>
-          <textarea
-            type="text"
-            value={content}
-            onChange={(e) => {
-              setContent(e.target.value);
-            }}
-          ></textarea>
-        </ProblemBox>
-        <Link to="/">
-          <RegisterBtn
-            onClick={() => {
-              onCreate();
-            }}
-          >
-            Post your question
-          </RegisterBtn>
-        </Link>
-        <CancleBtn>Discard draft</CancleBtn>
-      </QuestionForm>
-    </StyledAskQuestion>
+    <>
+      <StyledAskQuestion>
+        <QuestionForm>
+          <QuestionHeader>
+            <QuestionH1>Ask a public question</QuestionH1>
+            <img src={backimg} alt="backimg"></img>
+          </QuestionHeader>
+          <TitleBox>
+            <Title>Title</Title>
+            <Desc>
+              Be specific and imagine you’re asking a question to another
+              person.
+            </Desc>
+            <TitleInput
+              type="text"
+              placeholder="e.g. Is there an R function for finding the index of element in a vector?"
+              value={title}
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            ></TitleInput>
+          </TitleBox>
+          <ProblemBox>
+            <Title>What are the details of your problem?</Title>
+            <Desc>
+              Introduce the problem and expand on what you put in the title.
+              Minimum 20 characters.
+            </Desc>
+            <textarea
+              type="text"
+              value={content}
+              onChange={(e) => {
+                setContent(e.target.value);
+              }}
+            ></textarea>
+          </ProblemBox>
+          <Link to="/">
+            <RegisterBtn
+              onClick={() => {
+                onCreate();
+              }}
+            >
+              Post your question
+            </RegisterBtn>
+          </Link>
+          <CancleBtn>Discard draft</CancleBtn>
+        </QuestionForm>
+      </StyledAskQuestion>
+      <Footer />
+    </>
   );
 };
 
