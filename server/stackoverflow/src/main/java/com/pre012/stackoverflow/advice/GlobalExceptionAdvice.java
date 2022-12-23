7a75044a02cd19907e33dde8004b94e613d8e1dc
Jonghui <68373532+hie6953@users.noreply.github.com>
2022-12-23 22:12:41 +0900

    feat: URI 유효성 검사 예외처리 추가 유효성 검사 실패시 상태 코드 및 response

---
+++ server/stackoverflow/src/main/java/com/pre012/stackoverflow/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,28 @@
+package com.pre012.stackoverflow.advice;
+
+import com.pre012.stackoverflow.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getBindingResult());
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException(ConstraintViolationException e) {
+        final ErrorResponse response = ErrorResponse.of(e.getConstraintViolations());
+        return response;
+    }
+
+}
---
+++ server/stackoverflow/src/main/java/com/pre012/stackoverflow/response/ErrorResponse.java
@@ -0,0 +1,74 @@
+package com.pre012.stackoverflow.response;
+
+import lombok.Getter;
+import org.springframework.validation.BindingResult;
+
+
+import javax.validation.ConstraintViolation;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Getter
+public class ErrorResponse {
+    private List<FieldError> fieldErrors; // 유효성 검사 실패시 에러 핸들링
+    private List<ConstraintViolationError> violationErrors; // URI 변수 값 유효성 검사 실패시 에러 핸들링
+    private ErrorResponse(List<FieldError> fieldErrors, List<ConstraintViolationError> violationErrors) {
+        this.fieldErrors = fieldErrors;
+        this.violationErrors = violationErrors;
+    }
+
+    public static ErrorResponse of(BindingResult bindingResult) {
+        return new ErrorResponse(FieldError.of(bindingResult), null);
+    }
+
+    public static ErrorResponse of(Set<ConstraintViolation<?>> violations) {
+        return new ErrorResponse(null, ConstraintViolationError.of(violations));
+    }
+
+    @Getter
+    public static class FieldError {
+        private String field;
+        private String reason;
+
+        private FieldError(String field, String reason) {
+            this.field = field;
+            this.reason = reason;
+        }
+
+        public static List<FieldError> of(BindingResult bindingResult) {
+            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getDefaultMessage()))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    public static class ConstraintViolationError {
+        private String propertyPath;
+        private Object rejectedValue;
+        private String reason;
+
+        private ConstraintViolationError(String propertyPath, Object rejectedValue,
+                                         String reason) {
+            this.propertyPath = propertyPath;
+            this.rejectedValue = rejectedValue;
+            this.reason = reason;
+        }
+
+        public static List<ConstraintViolationError> of(
+                Set<ConstraintViolation<?>> constraintViolations) {
+            return constraintViolations.stream()
+                    .map(constraintViolation -> new ConstraintViolationError(
+                            constraintViolation.getPropertyPath().toString(),
+                            constraintViolation.getInvalidValue().toString(),
+                            constraintViolation.getMessage()
+                    )).collect(Collectors.toList());
+        }
+    }
+
+
+}
