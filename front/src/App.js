542ea9a57924082b674e174be40790bf62d1edab
rhino-ty <okko8522@gmail.com>
2022-12-23 09:17:11 +0900

    refactor: useFetch 데이터 Props로 변경
    다양한 페이지로 펼쳐주게끔 App.js 에 작성

--- front/src/App.js
+++ front/src/App.js
@@ -6,20 +6,26 @@ import Header from "./components/Header";
 import QuestionDetail from "./pages/QuestionDetail";
 import AskQuestion from "./pages/AskQuestion";
 import QuestionEdit from "./pages/QuestionEdit";
+import useFetch from "./util/useFetch";
 
 const StyledApp = styled.div`
   margin-top: 50px;
 `;
 
 function App() {
+  const url = "http://localhost:3001/Question";
+  const { questionData, loading } = useFetch(url);
   return (
     <BrowserRouter>
       <Header />
       <StyledApp>
         <Routes>
-          <Route path="/" element={<Domain />} />
+          <Route path="/" element={<Domain questionData={questionData} loading={loading} />} />
           <Route path="/ask" element={<AskQuestion />} />
-          <Route path="/question" element={<QuestionDetail />} />
+          <Route
+            path="/question"
+            element={<QuestionDetail questionData={questionData} loading={loading} />}
+          />
           <Route path="/edit" element={<QuestionEdit />} />
           {/* <Route path="/" element={<Domain />} /> */}
         </Routes>
