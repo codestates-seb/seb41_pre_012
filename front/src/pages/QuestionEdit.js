8ba0248f5d3c750fadf44d274ac523f60f23ece5
LEEKH109 <dlrjsgml109@gmail.com>
2022-12-25 17:09:14 +0900

    fix: 폰트,샘플 텍스트 수정, 최종 마감

--- front/src/pages/QuestionEdit.js
+++ front/src/pages/QuestionEdit.js
@@ -8,6 +8,7 @@ import { questionUpdate } from "../util/questionAPI";
 const Container = styled.div`
   width: 100%;
   max-width: 1264px;
+  height: auto;
   display: flex;
   justify-content: space-between;
   margin: 0 auto;
@@ -16,7 +17,6 @@ const StyledSection = styled.section`
   max-width: 1100px;
   width: calc(100% - 164px);
   padding: 24px;
-  height: 1080px;
   border-left: 1px solid #d7d9dc;
   display: flex;
   flex-direction: column;
@@ -33,27 +33,38 @@ const EditBox = styled.div`
 const TipBox = styled.div`
   width: 100%;
   height: 120px;
+  padding: 10px;
   background: #fdf7e2;
   border-radius: 3px;
   border: 2px solid #e6cf79;
+`;
+const Tipp = styled.p`
+  margin: 5px 0 0 5px;
+  font-size: 15px;
 `;
 const TextAreaName = styled.div`
   font-size: 18px;
   margin-top: 20px;
 `;
-const TextArea = styled.textarea`
+const TextArea = styled.input`
   width: 100%;
-  height: 200px;
   color: #18191a;
   border-radius: 5px;
+  border: 1px solid #e3e6e8;
   padding: 10px;
 `;
-const MainArea = styled(TextArea)`
-  height: 200px;
+const MainArea = styled.textarea`
+  width: 100%;
+  height: 250px;
+  color: #18191a;
+  border-radius: 5px;
+  border: 1px solid #e3e6e8;
+  padding: 10px;
+  overflow: scroll;
 `;
 const ResultArea = styled.div`
   margin-top: 20px;
-  height: 300px;
+  height: auto;
 `;
 const ResultTitle = styled.div`
   font-size: 2.07692308rem;
@@ -103,13 +114,13 @@ const QuestionEdit = () => {
   /*id는 페이지 라우팅에 활용해야 되기때문에 params로 따로 받아 온다*/
   const location = useLocation();
   const id = location.state.id;
-  const testTitle = location.state.title;
-  const testContent = location.state.content;
-  console.log(testTitle);
-  console.log(testContent);
+  const title = location.state.title;
+  const content = location.state.content;
+  console.log(title);
+  console.log(content);
 
-  const [editTitle, setEditTitle] = useState(`${testTitle}`);
-  const [editContent, setEditContent] = useState(`${testContent}`);
+  const [editTitle, setEditTitle] = useState(`${title}`);
+  const [editContent, setEditContent] = useState(`${content}`);
   /*fake 서버 양식에 맞추어 작성*/
   /*기존 데이터를 받아와서 화면 출력 후 편집 하고 싶다면 정보를 받아와야 한다. 어떻게?*/
 
@@ -128,7 +139,18 @@ const QuestionEdit = () => {
         <StyledSection>
           <MainSection>
             <EditBox>
-              <TipBox>sample</TipBox>
+              <TipBox>
+                <Tipp>
+                  Your edit will be placed in a queue until it is peer reviewed.
+                </Tipp>
+                <Tipp>
+                  We welcome edits that make the post easier to understand and
+                  more valuable for readers. Because community members review
+                  edits, please try to make the post substantially better than
+                  how you found it, for example, by fixing grammar or adding
+                  additional resources and hyperlinks.
+                </Tipp>
+              </TipBox>
               <TextAreaName>Title</TextAreaName>
               <TextArea
                 type="text"
@@ -155,7 +177,7 @@ const QuestionEdit = () => {
                 <Link to={`/question/${id}`}>
                   <SaveButton
                     onClick={() => {
-                      !(editTitle === "")
+                      !(editTitle === "" || editContent === "")
                         ? onEdit()
                         : alert("빈 문항이 없어야 합니다.");
                     }}
