6ca2e0d6b19eacec93e8a79b5ad49b6bbee0aaa9
LEEKH109 <dlrjsgml109@gmail.com>
2022-12-26 11:24:22 +0900

    feat: 디자인 완성

--- front/src/pages/MyPage.js
+++ front/src/pages/MyPage.js
@@ -15,10 +15,10 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   margin: 1 auto;
-  background: gray;
+  padding-left: 10px;
+  background: #d9d9d9;
 `;
 const UserBox = styled.div`
-  background: red;
   width: 100%;
   height: 20%;
   display: flex;
@@ -26,7 +26,6 @@ const UserBox = styled.div`
 const UserImgBox = styled.div`
   width: 20%;
   height: 100%;
-  background: green;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -38,7 +37,6 @@ const UserImg = styled.img`
 const UserNameBox = styled.div`
   width: 80%;
   height: 100%;
-  background: purple;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -54,9 +52,17 @@ const UserFunction = styled.p`
   font-size: 20px;
   margin-top: 5px;
 `;
+const UserButton = styled.button`
+  background: inherit;
+  border: none;
+  box-shadow: none;
+  border-radius: 0;
+  padding: 0;
+  overflow: visible;
+  cursor: pointer;
+`;
 
 const StatsDetailBox = styled.div`
-  background: blue;
   width: 100%;
   height: 80%;
   display: flex;
@@ -65,25 +71,35 @@ const StatsDetailBox = styled.div`
 const StatsBox = styled.div`
   width: 35%;
   height: 100%;
-  border: 1px solid black;
 `;
 const DetailBox = styled.div`
   width: 65%;
   height: 100%;
-  border: 1px solid black;
 `;
 const QusetionAnswerBox = styled.div`
   width: 100%;
   height: 50%;
-  border: 1px solid black;
+  padding: 10px;
 `;
 const TextBox = styled.div`
   width: 100%;
-  height: 20%;
-  background: gray;
+  height: 15%;
+  padding-left: 5px;
+  font-size: 25px;
+  display: flex;
+  align-items: center;
 `;
 const TextStatBox = styled(TextBox)`
-  height: 10%;
+  height: 5%;
+`;
+const UserDataBox = styled.div`
+  width: 100%;
+  height: 80%;
+  border-radius: 5px;
+  background: white;
+`;
+const UserStatBox = styled(UserDataBox)`
+  height: 20%;
 `;
 
 const MyPage = () => {
@@ -99,20 +115,26 @@ const MyPage = () => {
             <UserNameBox>
               <UserValue>
                 <UserName>User1234</UserName>
-                <UserFunction>logout | edit</UserFunction>
+                <UserFunction>
+                  <UserButton>logout</UserButton> |{" "}
+                  <UserButton>edit</UserButton>
+                </UserFunction>
               </UserValue>
             </UserNameBox>
           </UserBox>
           <StatsDetailBox>
             <StatsBox>
-              <TextStatBox></TextStatBox>
+              <TextStatBox>Stats</TextStatBox>
+              <UserStatBox></UserStatBox>
             </StatsBox>
             <DetailBox>
               <QusetionAnswerBox>
-                <TextBox></TextBox>
+                <TextBox>Answers</TextBox>
+                <UserDataBox></UserDataBox>
               </QusetionAnswerBox>
               <QusetionAnswerBox>
-                <TextBox></TextBox>
+                <TextBox>Questions</TextBox>
+                <UserDataBox></UserDataBox>
               </QusetionAnswerBox>
             </DetailBox>
           </StatsDetailBox>
