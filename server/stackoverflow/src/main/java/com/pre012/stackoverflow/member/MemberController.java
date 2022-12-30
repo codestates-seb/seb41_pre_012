976c55f50b14288c103d2cd650015807b8c4627a
lhj920514 <lhj925014@gmail.com>
2022-12-30 21:14:16 +0900

    Feat:마이페이지구현

--- server/stackoverflow/src/main/java/com/pre012/stackoverflow/member/MemberController.java
+++ server/stackoverflow/src/main/java/com/pre012/stackoverflow/member/MemberController.java
@@ -5,9 +5,12 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Api(tags = {"회원"})
 @RestController
@@ -31,9 +34,39 @@ public class MemberController {
 
     @ApiOperation(value="회원 삭제", notes="회원 정보를 삭제한다.")
     @ApiImplicitParam(name = "member-id", value = "회원 정보", paramType = "path")
-    @DeleteMapping("/{member-id}")
-    public ResponseEntity deleteMember(@PathVariable("member-id") Long mid) {
-        memberService.deleteMember(mid);
+    @DeleteMapping("/{mid}")
+    public ResponseEntity deleteMember(@PathVariable("mid") long mid,
+                                       @AuthenticationPrincipal String email) {
+        memberService.deleteMember(mid, email);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
+
+
+    @PatchMapping("/{mid}")
+    @ApiOperation(value = "회원정보 수정", notes = "회원정보 수정 API")
+    public ResponseEntity patchAnswer(@PathVariable("mid") long mid,
+                                      @Valid @RequestBody MemberPatchDto memberPatchDto,
+                                      @AuthenticationPrincipal String email) {
+        Member member = memberMapper.memberPatchDtoToMember(memberPatchDto);
+        member.setMid(mid);
+        Member response = memberService.updateMember(member, email);
+
+        return new ResponseEntity<>(memberMapper.memberToMemberResponseDto(response), HttpStatus.OK);
+
+    }
+
+
+    @GetMapping("/{mid}")
+    public ResponseEntity getAnswer(@PathVariable("mid") long mid,
+                                    @AuthenticationPrincipal String email){
+        Member member = memberService.getMember(mid, email);
+        MemberDetailResponseDto response = memberMapper.memberToMemberDetailResponseDto(member);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+
+
+
 }
