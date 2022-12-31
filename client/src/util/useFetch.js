09dd89eec1714546491a825b9da29d07df65b2f8
rhino-ty <okko8522@gmail.com>
2022-12-31 12:17:51 +0900

    fix: id 추가 및 조건 분기 추가
    Co-authored-by: LEEKH109 <LEEKH109@users.noreply.github.com>

--- client/src/util/useFetch.js
+++ client/src/util/useFetch.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 const url = "http://localhost:3001/Question";
 
-function useFetch() {
+function useFetch(id) {
   //null설정한 이유: 모든 data가 같진 않기 때문
   const [questionData, setQuestionData] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -12,8 +12,13 @@ function useFetch() {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const response = await axios.get(url);
-        setQuestionData(response.data);
+        if (id) {
+          const response = await axios.get(`${url}/${id}`);
+          setQuestionData(response.data);
+        } else {
+          const response = await axios.get(`${url}`);
+          setQuestionData(response.data);
+        }
       } catch (err) {
         setError(err);
       } finally {
