4a35ad205d67df1c989832401ec45ac31aa0abed
Jonghui <68373532+hie6953@users.noreply.github.com>
2023-01-01 13:37:19 +0900

    feat: mysql 추가 및 h2 제거

--- server/stackoverflow/src/test/java/com/pre012/stackoverflow/StackoverflowApplicationTests.java
+++ server/stackoverflow/src/test/java/com/pre012/stackoverflow/StackoverflowApplicationTests.java
@@ -3,7 +3,7 @@ package com.pre012.stackoverflow;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class StackoverflowApplicationTests {
 
     @Test
